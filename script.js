df34ad226d382e8718ad8eb7918639f6b9d828c7
H4XF3T <101671125+H4xf3t@users.noreply.github.com>
2024-11-21 19:50:49 -0500

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
 // Importa y configura Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
 
 // Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID",
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAWvLVI64C1LVC1gkbM38fMgrWQUZJr3b4",
+    authDomain: "login-948d9.firebaseapp.com",
+    projectId: "login-948d9",
+    storageBucket: "login-948d9.firebasestorage.app",
+    messagingSenderId: "1017210831314",
+    appId: "1:1017210831314:web:a317cd78bb0320d008080b",
+    measurementId: "G-4TNH10J8DX"
+  };
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-// Manejo del formulario de inicio de sesión
-document.getElementById('loginForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  // Inicio de sesión con Firebase Authentication
-  signInWithEmailAndPassword(auth, username, password)
-    .then((userCredential) => {
-      // Inicio de sesión exitoso
-      const user = userCredential.user;
-      alert(`Bienvenido, ${user.email}!`);
-      console.log("Usuario autenticado:", user);
-    })
-    .catch((error) => {
-      // Manejo de errores
-      console.error("Error al iniciar sesión:", error);
-      document.getElementById('errorMessage').innerText = "Usuario o contraseña incorrectos.";
-    });
-});
+</script>
